a873dda93aa16d3a7bfb18265ea1ba977cb52cbf
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2024-08-17 12:04:14 -0600

    Did a mess with the appbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BasicButton from './components/button';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
     return (
         <div>
+            <Navbar />
             <h1><center>React 18 Alpha</center></h1>
             <BasicButton>Click me</BasicButton>
         </div>
